44a89731bcc72b40bee8058c301cbb1ab5c9411f
Tilakkumar13 <tilakkumar.hvs@gmail.com>
2026-07-17 14:18:38 +0200

    chore: remove debug logging from google-sheets after diagnosis

--- lib/google-sheets.ts
+++ lib/google-sheets.ts
@@ -2,14 +2,7 @@ import { google } from "googleapis";
 
 function getGoogleAuth() {
   const clientEmail = process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL;
-  const rawKey = process.env.GOOGLE_PRIVATE_KEY || "";
-  const privateKey = rawKey.replace(/\\n/g, "\n");
-
-  console.log("[google-sheets] clientEmail present:", !!clientEmail);
-  console.log("[google-sheets] rawKey length:", rawKey.length);
-  console.log("[google-sheets] rawKey first 30 chars:", rawKey.substring(0, 30));
-  console.log("[google-sheets] privateKey starts with BEGIN:", privateKey.startsWith("-----BEGIN"));
-  console.log("[google-sheets] privateKey contains real newlines:", privateKey.includes("\n"));
+  const privateKey = process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n");
 
   if (!clientEmail || !privateKey) {
     throw new Error("Google Sheets credentials are not configured.");
